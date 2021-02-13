12a33c8a18a1f017a0e837ad9b93b055ba777250
Mental <73140396+MaximKing1@users.noreply.github.com>
2021-02-13 18:02:06 +0000

    New Stuff

--- Client.js
+++ Client.js
@@ -1,16 +1,18 @@
 class Client {
     constructor(client, whKeys, port) {
         if (typeof client != "string") {
-            throw new TypeError('[BLWEBHOOKS] Client is not defined');
+            return console.log('[BLWEBHOOKS] Client is not defined');
         } else if (typeof port != "number") {
-            throw new TypeError('[BLWEBHOOKS] Port Number is not defined');
-        }else if (typeof whKeys != "number") {
-            throw new TypeError('[BLWEBHOOKS] Port Number is not defined');
+            return console.log('[BLWEBHOOKS] Port Number is not defined');
         }
     }
 
-    async fetchLink(link_id) {
-
+    async setLogging(toggle) {
+        if (toggle == true) {
+            await console.log('[BLWEBHOOKS] Advance Logging Enabled');
+        } else if (toggle == false) {
+            await console.log('[BLWEBHOOKS] Advance Logging Disabled');
+        }
     }
 }
 
