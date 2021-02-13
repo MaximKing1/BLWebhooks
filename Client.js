f451f2e0447f6632c02c1c1764de96c1f1364c93
Mental <73140396+MaximKing1@users.noreply.github.com>
2021-02-13 18:34:38 +0000

    Added Chalk

--- Client.js
+++ Client.js
@@ -1,29 +1,30 @@
 const express = require('express');
 const app = express();
+const chalk = require('chalk');
 
 class Client {
     constructor(client, whKeys, port) {
         if (!client) {
-            return console.log('[BLWEBHOOKS] The client is not defined')
+            return console.log(chalk.red('[BLWEBHOOKS] The client is not defined'))
         } else if (!whKeys) {
-            return console.log('[BLWEBHOOKS] The whKeys are not defined')
+            return console.log(chalk.red('[BLWEBHOOKS] The whKeys are not defined'))
         } else if (typeof port != "number") {
-            return console.log('[BLWEBHOOKS] The Port Number is not defined');
+            return console.log(chalk.red('[BLWEBHOOKS] The Port Number is not defined'));
         }
         if(port) {
             app.listen(port);
-            console.log(`[BLWEBHOOKS] The Vote Webserver Has Started On Port ${port}.`);
+            console.log(chalk.green(`[BLWEBHOOKS] The Vote Webserver Has Started On Port ${port}.`));
         }
         if(whKeys) {
-            console.log(`[BLWEBHOOKS] Webhook Keys Have Been Registered!`);
+            console.log(chalk.green(`[BLWEBHOOKS] Webhook Keys Have Been Registered!`));
         }
     }
 
     async setLogging(toggle) {
         if (toggle == true) {
-            await console.log('[BLWEBHOOKS] Advance Logging Enabled');
+            await console.log(chalk.green('[BLWEBHOOKS] Advance Logging Enabled'));
         } else if (toggle == false) {
-            await console.log('[BLWEBHOOKS] Advance Logging Disabled');
+            await console.log(chalk.red('[BLWEBHOOKS] Advance Logging Disabled'));
         }
     }
 }
