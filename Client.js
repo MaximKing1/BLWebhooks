bfb5de44798323b90ae09da9b9ab500cd0e93b3a
Mental <73140396+MaximKing1@users.noreply.github.com>
2021-02-13 23:00:14 +0000

    Slow Down Spam Requests

--- Client.js
+++ Client.js
@@ -5,6 +5,17 @@ const bodyParser = require('body-parser');
 const helmet = require('helmet');
 
 // Brute Force Protection
+const slowDown = require("express-slow-down");
+//app.enable("trust proxy");
+const speedLimiter = slowDown({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  delayAfter: 100, // allow 100 requests per 15 minutes, then...
+  delayMs: 500 // begin adding 500ms of delay per request above 100:
+  // request # 101 is delayed by  500ms
+  // request # 102 is delayed by 1000ms
+  // request # 103 is delayed by 1500ms
+  // etc.
+});
 
 const rateLimit = require("express-rate-limit");
 // app.set('trust proxy', 1);
