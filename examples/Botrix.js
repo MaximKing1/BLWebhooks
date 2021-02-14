f8c781a402d8f799e94034b2cbccf39149603cfe
Mental <73140396+MaximKing1@users.noreply.github.com>
2021-02-14 09:11:03 +0000

    Update Botrix.js

--- examples/Botrix.js
+++ examples/Botrix.js
@@ -1,3 +1,11 @@
+const discord = require("discord.js");
+const client = discord.Client();
+
+const blwebhooks = require("blwebhooks");
+
+// Attatch The Client and Port to the module.
+// The client is it attaching to the client, 80 is the port it will run on
+const bl = new blwebhooks.Client(client, 80);
 
 
 const Botrix = bl.BotrixVoteHook("Botrix", "LOADS_OF_RANDOMNESS", true);
