53d46d3000f8c68ed6c10798543ec9b9b17f63c4
Mental <73140396+MaximKing1@users.noreply.github.com>
2021-02-14 09:10:28 +0000

    Update Botrix.js

--- examples/Botrix.js
+++ examples/Botrix.js
@@ -1,1 +1,8 @@
 
+
+const Botrix = bl.BotrixVoteHook("Botrix", "LOADS_OF_RANDOMNESS", true);
+
+// This code will run after a new vote was received from Botrix
+BLWEvent.on('BTR-voted', function async (userID) {
+  console.log(`${userID} Voted!`)
+})
