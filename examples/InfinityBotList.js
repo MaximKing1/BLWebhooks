f101f5c183257db0c68df946fbddf55c61b650e0
Mental <73140396+MaximKing1@users.noreply.github.com>
2021-02-14 19:54:38 +0000

    Update InfinityBotList.js

--- examples/InfinityBotList.js
+++ examples/InfinityBotList.js
@@ -5,11 +5,12 @@ const { WebhooksManager } = require("blwebhooks");
 
 // Attatch The Client and Port to the module.
 // The client is it attaching to the client, 80 is the port it will run on
-const bl = new blwebhooks.Client(client, 80);
+const voteClient = new WebhooksManager(client, 80);
+client.voteManager = voteClient;
 
-bl.extraProtection(true);
+voteManager.extraProtection(true);
 
-const InfinityBotList = bl.IBLVoteHook("IBLHook", "LOADS_OF_RANDOMNESS", true);
+const InfinityBotList = voteManager.IBLVoteHook("IBLHook", "LOADS_OF_RANDOMNESS", true);
 
 // This code will run after a new vote was received from InfinityBotList
 BLWEvent.on('IBL-voted', async function (userID, botID, type, timeStamp) {
