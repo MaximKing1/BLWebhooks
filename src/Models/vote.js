d52aa968332c1a6e2c4734b456cbfc26c4a73880
Mental <73140396+MaximKing1@users.noreply.github.com>
2021-02-16 12:23:37 +0000

    Update vote.js

--- src/Models/vote.js
+++ src/Models/vote.js
@@ -1,1 +1,8 @@
+const mongoose = require("mongoose");
 
+const userSchema = mongoose.Schema({
+  userID: { type: String, required: true },
+  totalVotes: { type: Number, default: "0" }
+});
+
+module.exports = mongoose.model('userVote', userSchema);
