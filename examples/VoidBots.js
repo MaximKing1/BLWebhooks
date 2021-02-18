0f3f47da2576eda88b0a04fd67d857240cf70593
Mental <73140396+MaximKing1@users.noreply.github.com>
2021-02-18 23:19:54 +0000

    Update VoidBots.js

--- examples/VoidBots.js
+++ examples/VoidBots.js
@@ -13,19 +13,19 @@ voteManager.extraProtection(true);
 const VoidBots = voteManager.VoidBotsVoteHook("VBHOOK", "LOADS_OF_RANDOMNESS", true);
 
 // This code will run after a new vote was received from InfinityBotList
-BLWEvent.on('VB-voted', async function (userID, botID, type, timeStamp) {
+client.on('VB-voted', async function (userID, botID, type, timeStamp) {
   console.log(userID)
   
   // Add role for 24 hours
   const guildD = client.guilds.cache.get("758641373074423808")
    guildD.members.fetch(userID).catch(() => null).then(member => {
      
-       if (!member) return;
+      if (!member) return;
      
       member.roles.add("748340144897261660");
      
       setTimeout(() => member.roles.remove("748739144897261660").catch(console.error), 1000 * 60 * 60 * 24);
      
 }).catch(() => {});
-})
-})
+
+});
