0feb01bdcd1169e4453336456e4d86edd693c52d
Mental <73140396+MaximKing1@users.noreply.github.com>
2021-02-20 20:26:09 +0000

    .

--- src/Client.js
+++ src/Client.js
@@ -33,6 +33,9 @@ class WebhooksManager extends EventEmitter {
         console.log(chalk.red("-----------------------"));
         console.log("The client has been changed to WebhooksManager, check our docs if your code is not working + BLWEvent.on Changed To client.on");
         console.log(chalk.red("----------------------"));
+        console.log(chalk.red("-----------------------"));
+        console.log("All Manager Options Updated Check Our Docs Now! https://github.com/MaximKing1/BLWebhooks#readme");
+        console.log(chalk.red("----------------------"));
 
         /**
          * The Discord Client
